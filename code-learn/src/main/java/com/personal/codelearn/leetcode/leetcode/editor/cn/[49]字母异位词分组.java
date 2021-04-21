747bca79798358977d925431147aa539ba5a3a20
“yangbao.yang” <yangbao.yang@alibaba-inc.com>
2021-04-21 17:04:05 +0800

    feature(增加字母以为分组方法):
    主要使用map去,重排序原来的str,因为只有小写字母

---
+++ code-learn/src/main/java/com/personal/codelearn/leetcode/leetcode/editor/cn/[49]字母异位词分组.java
@@ -0,0 +1,66 @@
+package com.personal.codelearn.leetcode.leetcode.editor.cn;
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串 
+// 👍 725 👎 0
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        if (strs == null) {
+            return res;
+        }
+        Map<List<Integer>, List<String>> resMap = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            List<Integer> keyList = new ArrayList<>();
+            for (char c : chars) {
+                keyList.add(Integer.valueOf(c));
+            }
+            Collections.sort(keyList);
+
+            if (resMap.containsKey(keyList)) {
+                List<String> list = resMap.get(keyList);
+                list.add(str);
+                resMap.put(keyList, list);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                resMap.put(keyList, list);
+            }
+        }
+        res.addAll(resMap.values());
+        return res;
+    }
+
+    //public static void main(String[] args) {
+    //    String[] s = new String[] {"cab","tin","pew","duh","may","ill","buy","bar","max","doc"};
+    //    //String[] s = new String[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+    //    Solution49 so = new Solution49();
+    //    System.out.println(JSON.toJSONString(so.groupAnagrams(s)));
+    //}
+}
+//leetcode submit region end(Prohibit modification and deletion)
