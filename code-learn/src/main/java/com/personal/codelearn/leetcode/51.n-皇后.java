612f9a61691cb9ea01e76322228e16c95b258203
“yangbao.yang” <yangbao.yang@alibaba-inc.com>
2021-12-16 15:23:56 +0800

    提交新的解决

--- code-learn/src/main/java/com/personal/codelearn/leetcode/51.n-皇后.java
+++ code-learn/src/main/java/com/personal/codelearn/leetcode/51.n-皇后.java
@@ -9,9 +9,10 @@ package com.personal.codelearn.leetcode;
 // @lc code=start
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 
-class Solution222 {
+class Solution52 {
     public List<List<String>> solveNQueens(int n) {
         // 判断是否超出了界限范围
         if (n > 9 || n < 0) {
@@ -24,13 +25,86 @@ class Solution222 {
                 queenArr[i][j] = ".";
             }
         }
+        System.out.println(JSON.toJSONString(queenArr));
         // 利用回溯法进行处理
+        putQueen(queenArr, 0, 0);
+        System.out.println(JSON.toJSONString(queenArr));
         return null;
 
     }
 
+    public void putQueen(String[][] queenArr, int i, int j) {
+        if (j < queenArr.length) {
+            putQueen(queenArr, i, j++);
+        }
+        if (i < queenArr.length) {
+            putQueen(queenArr, i++, j);
+        }
+        boolean canPut = canPutQueen(queenArr, i, j);
+        if (canPut) {
+            queenArr[i][j] = "Q";
+        } else {
+            queenArr[i][j] = ".";
+        }
+    }
+
+    private boolean canPutQueen(String[][] queenArr, int h, int s) {
+        // 检查横
+        for (int i = 0; i < queenArr.length; i++) {
+            if (queenArr[i][s] == "Q") {
+                return false;
+            }
+        }
+        // 检查竖
+        for (int i = 0; i < queenArr.length; i++) {
+            if (queenArr[h][i] == "Q") {
+                return false;
+            }
+        }
+        // 检查左斜
+        int h1 = h;
+        int s1 = s;
+        while (h1 >= 0 && s1 >= 0) {
+            if (isQueen(queenArr, h1, s1)) {
+                return false;
+            }
+            h1--;
+            s1--;
+        }
+        while (h1 < queenArr.length && s1 < queenArr.length) {
+            if (isQueen(queenArr, h1, s1)) {
+                return false;
+            }
+            h1++;
+            s1++;
+        }
+        // 检查右斜
+        int h2 = h;
+        int s2 = s;
+        while (h2 >= 0 && s2 < queenArr.length) {
+            if (isQueen(queenArr, h2, s2)) {
+                return false;
+            }
+            h2--;
+            s2++;
+        }
+        while (h2 < queenArr.length && s2 >= 0) {
+            if (isQueen(queenArr, h2, s2)) {
+                return false;
+            }
+            h2++;
+            s2--;
+        }
+        return true;
+    }
+
+    private boolean isQueen(String[][] queenArr, int h, int s) {
+        return "Q".equals(queenArr[h][s]);
+    }
+
     public static void main(String[] args) {
-        System.out.println("n queens");
+        // Solution so = new Solution();
+        // so.solveNQueens(4);
     }
 }
 // @lc code=end
---
+++ code-learn/src/main/java/com/personal/codelearn/leetcode/53.最大子数组和.java
@@ -0,0 +1,53 @@
+package com.personal.codelearn.leetcode;
+
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+            // 此处是解决问题的关键，最大的子数之和不能加出来负数
+            // 所以当加出来是负数的时候就应该对其抛弃，即只需要一次遍历即可
+            if (sum < 0) {
+                sum = 0;
+            }
+
+        }
+        return max;
+    }
+
+    /**
+     * 可解决，但是超出时间限制，需要更优的解决方案
+     * 
+     * @param nums 数组
+     * @return 结果
+     */
+    public int maxSubArray1(int[] nums) {
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            int tmpMax = nums[i];
+            max = tmpMax > max ? tmpMax : max;
+            for (int j = i + 1; j < nums.length; j++) {
+                tmpMax += nums[j];
+                max = tmpMax > max ? tmpMax : max;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution so = new Solution();
+        int[] nums = new int[] { -2, 1 };
+        System.out.println(so.maxSubArray(nums));
+    }
+}
+// @lc code=end
